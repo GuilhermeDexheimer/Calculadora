87a1020b9b87559609c7ba387806a5fb1ae4b896
GuilhermeDexheimer <guigui_dex@hotmail.com>
2025-04-27 20:30:44 -0300

    Melhoria

--- ## Calculadora.py
+++ ## Calculadora.py
@@ -3,9 +3,11 @@
 # Inserir valores
 valor_1 = int(input('Informe um número: '))
 valor_2 = int(input('Informe um número: '))
+
 # Cálculos
-print (valor_1 + valor_2)
-print (valor_1 - valor_2)
-print (valor_1 * valor_2)
-print (valor_1 / valor_2)
-print (valor_1 % valor_2)
+
+print(f'Soma de {valor_1} + {valor_2} = {valor_1 + valor_2}')
+print(f'Subtração de {valor_1} - {valor_2} = {valor_1 - valor_2}')
+print(f'Multiplicação de {valor_1} x {valor_2} = {valor_1 * valor_2}')
+print(f'Divisão de {valor_1} / {valor_2} = {valor_1 / valor_2}')
+print(f'Resto de {valor_1} % {valor_2} = {valor_1 % valor_2}')

79add6c22c4de95c13f8ab366849133d9449ac70
GuilhermeDexheimer <guigui_dex@hotmail.com>
2025-04-27 20:06:01 -0300

    Calculadora
    Forneça dois números para fazer cálculos de soma, subtração, multiplicação, divisão e resto (porcentagem).

---
+++ ## Calculadora.py
@@ -0,0 +1,11 @@
+## Calculadora
+
+# Inserir valores
+valor_1 = int(input('Informe um número: '))
+valor_2 = int(input('Informe um número: '))
+# Cálculos
+print (valor_1 + valor_2)
+print (valor_1 - valor_2)
+print (valor_1 * valor_2)
+print (valor_1 / valor_2)
+print (valor_1 % valor_2)
